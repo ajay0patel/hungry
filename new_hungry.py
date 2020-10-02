7befed5e89596a07cfbb8f2ba3bc089efec94874
ajay0patel <51459396+ajay0patel@users.noreply.github.com>
2020-10-02 21:09:25 +0530

    Create new_hungry.py
    if user enters 'no' or 'No'  then directly output will be  do homework.

---
+++ new_hungry.py
@@ -0,0 +1,10 @@
+hungry=input("are you hungry?")
+if(hungry =='No' or hungry == 'no'):
+    print("do homework") 
+else:
+    print("eat samosa")
+    print("eat pizza")
+    print("eat burger")
+  
+    
+   
